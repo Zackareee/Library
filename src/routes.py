cee57b98a409149565df2ff77e566eda23187398
ZacKendallKing <github@zackaree.com>
2022-12-15 22:16:05 +1000

    Added basic form

--- src/routes.py
+++ src/routes.py
@@ -1,19 +1,11 @@
 import os
 import sqlite3
-
-from flask import Blueprint, render_template
+from src.forms import BookForm
+from flask import Blueprint, render_template,request
 
 mainbp = Blueprint('main', __name__)
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"  # !! Only in development environment.
-
-discord = []
-
-if os.name == 'nt':
-    s = "#"
-else:
-    s = "-"
-
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -29,8 +21,15 @@ def add_header(response):
 
 @mainbp.route('/', methods=['GET', 'POST'])
 def index():
+    
+    create_book_form = BookForm()
     conn = get_db_connection()
     booksvar = conn.execute('SELECT * FROM books').fetchall()
-    conn.close()
-    return render_template('index.html', booksvar=booksvar)
+    conn.close()       
+    
+    if request.method == "POST": #Submitting a form
+        print("wow")
+        return render_template('index.html', BookForm=create_book_form)
+
+    return render_template('index.html', booksvar=booksvar, BookForm=create_book_form)
 
