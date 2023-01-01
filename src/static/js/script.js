bc8d49b601b4f528812ece648fa92eeaa9907712
ZacKendallKing <github@zackaree.com>
2023-01-01 17:29:15 +1000

    draggin -disabled

---
+++ src/static/js/script.js
@@ -0,0 +1,33 @@
+function enableDragSort(listClass) {
+    const sortableLists = document.getElementsByClassName(listClass);
+    Array.prototype.map.call(sortableLists, (list) => { enableDragList(list) });
+}
+
+function enableDragList(list) { Array.prototype.map.call(list.children, (item) => { enableDragItem(item) }); }
+
+function enableDragItem(item) {
+    item.setAttribute('draggable', true)
+    item.ondrag = handleDrag;
+    item.ondragend = handleDrop;
+}
+
+function handleDrag(item) {
+    const selectedItem = item.target,
+        list = selectedItem.parentNode,
+        x = item.clientX,
+        y = item.clientY;
+
+    selectedItem.classList.add('drag-sort-active');
+    let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y).closest("li");
+    console.log(swapItem)
+    if (list === swapItem.parentNode) {
+        swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+        list.insertBefore(selectedItem, swapItem);
+    }
+}
+
+function handleDrop(item) {
+    item.target.classList.remove('drag-sort-active');
+}
+
+(() => { enableDragSort('drag-sort-enable') })();
