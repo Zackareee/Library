e2f6164385c45810f0694712bcd0425b712d0d1b
ZacKendallKing <github@zackaree.com>
2023-01-01 14:59:12 +1000

    New Layout Edit book functionality added Removed book links

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import SubmitField, StringField, FieldList,FormField,RadioField,DecimalField
+from wtforms.fields import SubmitField, StringField, FieldList,FormField,RadioField,DecimalField,HiddenField
 from wtforms.validators import InputRequired,Optional
 from wtforms.fields.html5 import DateField
 import os
@@ -14,4 +14,10 @@ class BookForm(FlaskForm):
   name =  StringField(validators=[InputRequired()], render_kw={'class':'form-control', 'placeholder':'One Piece'})
   link =  StringField(render_kw={'class':'form-control', 'placeholder':'https://google'})
   volume = DecimalField(validators=[Optional()],render_kw={'class': 'form-control', 'placeholder':'001'})
-  submit = SubmitField('Test', render_kw={'class':'btn btn-primary'})
+  submit = SubmitField('Submit', render_kw={'class':'btn btn-primary'})
+
+class EditForm(FlaskForm):
+  Eid = HiddenField()
+  Ename =  StringField(validators=[InputRequired()], render_kw={'class':'form-control', 'placeholder':'One Piece' })
+  Evolume = DecimalField(validators=[Optional()],render_kw={'class': 'form-control', 'placeholder':'001'})
+  Esubmit = SubmitField('Submit', render_kw={'class':'btn btn-primary'})
